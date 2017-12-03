6c2abc0f079e05a9a465195af2a10cff2abea367
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-12-03 14:11:11 -0500

    add styling to sent by

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ const functions = require('firebase-functions');
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+
+  response.send("Hello from Firebase!");
+});
